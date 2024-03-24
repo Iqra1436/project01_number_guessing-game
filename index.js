6d438880c9546a61d3d48ebc8225d50cde6d18bc
Iqra Jahangir <iqrasheikh1436@gmail.com>
2024-03-24 11:50:59 +0500

    This guess the number game is a short TypeScript/Node.js project

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//  1) Computer will generate a ramdon numner
+//  2) User input for quessing numner
+// 3) Compareuser input with computer generated number show results
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "UserGuessdNumber",
+        type: "number",
+        message: "Please guess a number between 1-6",
+    },
+]);
+if (answers.UserGuessdNumber === randomNumber) {
+    console.log("Congratulation ! you guested  right number");
+}
+else {
+    console.log("Sorry ! you guested Wrong number");
+}
